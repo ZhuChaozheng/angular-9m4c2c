8859120632c582271c1a3d4bb426bf4e30f04cdc
ZhuChaozheng <370045744@qq.com>
2022-03-16 19:08:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9m4c2c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9m4c2c)
---
+++ src/demo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <t-cascader class="t-demo-cascader" :options="options" v-model="value" multiple clearable/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          label: '选项一',
+          value: '1',
+          children: [
+            {
+              label: '子选项一',
+              value: '1.1',
+            },
+            {
+              label: '子选项二',
+              value: '1.2',
+            },
+            {
+              label: '子选项三',
+              value: '1.3',
+            },
+          ],
+        },
+        {
+          label: '选项二',
+          value: '2',
+          children: [
+            {
+              label: '子选项一',
+              value: '2.1',
+            },
+            {
+              label: '子选项二',
+              value: '2.2',
+            },
+          ],
+        },
+      ],
+      value: ['1.1'],
+    };
+  },
+  methods: {
+    handleValueChange(e) {
+      this.value = e;
+    },
+  },
+};
+</script>
+<style scoped>
+.t-demo-cascader-title {
+  margin: 10px 0;
+}
+.t-demo-cascader + .t-demo-cascader {
+  margin-top: 16px;
+}
+</style>
